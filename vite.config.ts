ec79a341eb195e707db4b1ba31f43f63d91ed7d4
Karin Bhandari <86232652+karinbhandari007@users.noreply.github.com>
2025-08-10 13:11:37 +0545

    chore: added required packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import graphqlLoader from "vite-plugin-graphql-loader";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), graphqlLoader()],
 });
